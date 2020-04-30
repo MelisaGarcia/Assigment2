67b4c2ae25c2c00e646280a8a3930e614a890d4f
Melisa García <59666739+MelisaGarcia@users.noreply.github.com>
2020-04-29 21:36:02 -0700

    Add files via upload

---
+++ Assigment2/Assigment2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Assigment2
+//
+//  Created by Andrea Dancek on 2020-04-29.
+//  Copyright © 2020 Melisa Garcia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
